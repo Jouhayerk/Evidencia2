dbcf30a078c3412224a5aae1c5c6950bfd8fd8ee
Jouhayerk <Jouhayerk>
2025-03-23 21:33:00 -0600

    Initial Commit

---
+++ Evidencia2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Evidencia2
+//
+//  Created by Charlie Jouhayerk on 23/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Evidencia2Tests/Evidencia2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Evidencia2Tests.swift
+//  Evidencia2Tests
+//
+//  Created by Charlie Jouhayerk on 23/03/25.
+//
+
+import Testing
+@testable import Evidencia2
+
+struct Evidencia2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
